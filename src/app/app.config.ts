6cf52baa3519d6cc42e7bff61c143816da773918
Riccardo Zazza <riccardo.zazza@icloud.com>
2025-10-30 20:23:48 +0100

    Risoluzioni Problemi Calendar e Dashboard

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,20 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// 1. IMPORTA TUTTO QUELLO CHE SERVE
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideNativeDateAdapter } from '@angular/material/core';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    // 2. AGGIUNGI QUESTI TRE PROVIDER
+    provideHttpClient(),    // Per EventDataService e MoodService
+    provideAnimations(),    // Per MatDialog e MatDatepicker
+    provideNativeDateAdapter() // Per MatDatepicker
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -5,11 +5,15 @@ import {Login} from './components/login/login';
 import {Register} from './components/register/register';
 import {UiConfig} from './components/ui-config/ui-config';
 import {ErrorPage} from './components/error-page/error-page';
+import {CalendarViewComponent} from './components/calendar/calendar-view/calendar-view.component';
+import {EventModalComponent} from './components/calendar/event-modal/event-modal.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, Login, Register, UiConfig, ErrorPage],
+  imports: [RouterOutlet, Home, Login, Register, UiConfig, ErrorPage, CalendarViewComponent, EventModalComponent],
   templateUrl: './app.html',
+  standalone: true,
   styleUrl: './app.css'
 })
 export class App {
